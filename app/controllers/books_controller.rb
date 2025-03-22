978fbd7b2d5d467b42b175a05a40cbed924deb1b
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-03-22 21:47:58 +0900

    レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if @book.save!
+    if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id) 
     else
-      render :index
+      @books = Book.all
+      render :index, status: :unprocessable_entity
     end     
   end
 
@@ -24,6 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id) 
     else
       render :edit
@@ -33,6 +36,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     if @book.destroy
+      flash[:notice] = "Book was successfully deleted."
       redirect_to books_path 
     else
       @book = Book.new
